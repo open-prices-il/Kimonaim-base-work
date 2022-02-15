3938003ce8c9c8d9a855073d107d00bee3379c67
aritheman88 <65540935+aritheman88@users.noreply.github.com>
2022-02-15 19:54:51 +0200

    Create shufersalist_creator.sql

---
+++ shufersalist_creator.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "shufersalPrices" (
+	"filename"	TEXT,
+	"store_id"	TEXT,
+	"upload_date"	TEXT,
+	"PriceUpdateDate"	TEXT,
+	"ItemCode"	INT,
+	"ItemName"	TEXT,
+	"ManufacturerName"	TEXT,
+	"ManufactureCountry"	TEXT,
+	"ManufacturerItemDescription"	TEXT,
+	"UnitQty"	TEXT,
+	"Quantity"	TEXT,
+	"UnitOfMeasure"	TEXT,
+	"ItemPrice"	REAL,
+	"UnitOfMeasurePrice"	REAL,
+	"AllowDiscount"	INT
+)
